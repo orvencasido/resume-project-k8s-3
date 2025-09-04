a3d9339357dbf9130413a425fe8eb8da4628cce9
Ubuntu <ubuntu@ip-172-31-42-20.ap-southeast-1.compute.internal>
2025-09-04 02:59:17 +0000

    update

---
+++ cicd-deploy.groovy
@@ -0,0 +1,52 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'docker-kubectl-agent'
+        }
+    }
+
+    environment {
+        DOCKER_IMAGE = "orvencasido/resume-project-k8s-3"
+        VERSION = "${env.BUILD_NUMBER}"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                container('docker') {
+                    script {
+                        sh "docker build -t ${DOCKER_IMAGE}:${VERSION} ."
+                    }
+                }
+            }
+        }
+
+        stage('Push') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PW')]) {
+                    container('docker') {
+                        script {
+                            sh "echo ${DOCKER_PW} | docker login -u ${DOCKER_USER} --password-stdin"
+                            sh "docker push ${DOCKER_IMAGE}:${VERSION}"
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                container('kubectl') {
+                    script {
+                        sh """
+                            kubectl apply -f resume-deployment.yaml -n default
+                            kubectl apply -f resume-service.yaml -n default
+                            kubectl set image deployment/resume resume=${DOCKER_IMAGE}:${VERSION} -n default
+                            kubectl rollout status deployment/resume -n default
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
